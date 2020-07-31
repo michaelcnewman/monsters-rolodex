0a57ce15275c514c40ab46b13ce1e0687e842484
Newman <newman039@gmail.com>
2020-07-31 17:42:15 -0400

    Updates

---
+++ precache-manifest.fc9645e20adc0e40abd38d42b969f06e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672a2d90b25fa256730a94609c16f6a5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "79b4df6d907833c39b06",
+    "url": "/monsters-rolodex/static/css/main.89a26579.chunk.css"
+  },
+  {
+    "revision": "3a01784a45c53e1d077b",
+    "url": "/monsters-rolodex/static/js/2.8ecfa0fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8ecfa0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b4df6d907833c39b06",
+    "url": "/monsters-rolodex/static/js/main.190d2b7f.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
